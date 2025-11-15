cd578ee2b0e20016bb0511739d4b8ccaa120c43e
KaizenUT <dummarulloa2006@gmail.com>
2025-11-15 11:39:25 -0500

    Corregir CORS - todo en minúsculas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'https://Kaizenut.github.io'
+  origin: 'https://kaizenut.github.io'
 })); // Comunicacion entre B y F sin error en el navegador 
 app.use(express.json());
 
